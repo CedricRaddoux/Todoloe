ea9f0a00199576fffe1c4ba550240e9693220300
CedricRaddoux <r0669133@student.thomasmore.be>
2019-08-15 17:12:59 +0200

    Create nav.inc.php

---
+++ includes/nav.inc.php
@@ -0,0 +1,6 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand logo" href="index.php"><img src="images/logo.png" alt="todo logo"></a><br>
+    <a class="text-danger nav-link" href="lists.php">Add list</a><br>
+    <a class="text-danger nav-link" href="tasks.php">Add task</a><br>
+    <a class="text-danger nav-link" href="includes/logout.inc.php">Logout</a>
+</nav>

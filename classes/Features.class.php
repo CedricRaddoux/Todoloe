110794e9578f0de3574e962e6ce8c87cf6238c70
CedricRaddoux <r0669133@student.thomasmore.be>
2019-08-15 17:12:49 +0200

    Create Features.class.php

---
+++ classes/Features.class.php
@@ -0,0 +1,48 @@
+<?php
+
+include_once("includes/db.inc.php");
+
+class Features
+{
+    private $name;
+    private $list;
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function setName($name)
+    {
+        if($name=="") {
+            throw new Exception('Name can not be empty');
+        }
+        $this->name = $name;
+    }
+
+    public function getList()
+    {
+        return $this->list;
+    }
+
+    public function setList($list)
+    {
+        $this->list = $list;
+    }
+
+public function addList()
+    {
+        global $conn;
+        $statement = $conn->prepare("INSERT INTO lists(name) VALUES (:name)");
+        $statement->bindValue(":name", $this->getName());
+        $statement->execute();
+    }
+
+    public function getLists()
+    {
+        global $conn;
+        $statement = $conn->prepare("select * from lists");
+        $statement->execute();
+        return $statement->fetchAll();
+    }
+}

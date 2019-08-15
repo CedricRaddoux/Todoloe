c818fcc2c006cfb557d0537df677374e0eaf3260
CedricRaddoux <r0669133@student.thomasmore.be>
2019-08-15 17:13:01 +0200

    Update index.php

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+include_once("classes/Features.class.php");
+
+?><!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Todoloe</title>
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Karla|Lexend+Deca&display=swap" rel="stylesheet">
+    
+</head>
+<body>
+<?php include_once("includes/nav.inc.php"); ?><br>
+
+<div class="container">
+    
+</div>
+
+</body>
+</html>

040601228aa067a6a6a3c24792ecaa125eff6836
CedricRaddoux <r0669133@student.thomasmore.be>
2019-08-17 14:52:30 +0200

    Update list.php

--- list.php
+++ list.php
@@ -14,10 +14,7 @@ try {
             $list->addList();
 
         } elseif (isset($_POST['delete'])) {
-                $name2 = $_POST['list'];
-    
-                $list = new Feature();
-                $list->setName($name2);
+                $list = new Features();
                 $list->deleteList();
             }
         }
@@ -35,34 +32,36 @@ $feed = new Features();
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>List</title>
-    <link rel="stylesheet" href="css/reset.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
-</head>
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Karla|Lexend+Deca&display=swap" rel="stylesheet">
+    </head>
 <body>
 
-<?php include_once("includes/nav.inc.php"); ?><br>
+<?php include_once("includes/nav.inc.php"); ?>
 
 <div class="container">
-    <p class="alert-warning"><?php if (isset($error)) {
-    echo htmlspecialchars($error);
-} ?></p>
 
     <form action="" method="post">
 
         <h1>Add list</h1>
 
         <div class="form-group">
-            <label for="name">Name</label>
-            <input class="form-control" type="text" name="name" id="name">
+        <div class="error-popup"><?php if (isset($error)) {
+        echo htmlspecialchars($error);
+        } ?></div>
+            <label for="name">Enter the name of the list</label>
+            <input class="inputfields" type="text" name="name" id="name">
         </div>
 
-        <input class="btn btn-danger" type="submit" value="save" name="save"><br>
+        <button class="submitbutton" type="submit" name="save">Add list</button>
         <br>
 
         <div class="form-group">
             
                 <?php foreach ($feed->getLists() as $l): ?>
                     <p value="<?php echo htmlspecialchars($l["name"]); ?>"><?php echo htmlspecialchars($l["name"]); ?></p>
+                    <input class="btn btn-danger" type="submit" value="delete" name="delete">
                 <?php endforeach; ?>
             
         </div>
@@ -70,10 +69,5 @@ $feed = new Features();
         
         
     </form>
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
-<script type="text/javascript" src="js/script.js"></script>
-
 </body>
 </html>

fef00dff8223b24045d5265ccb897b1e7cf33444
CedricRaddoux <r0669133@student.thomasmore.be>
2019-08-18 21:06:59 +0200

    Update User.class.php

--- classes/User.class.php
+++ classes/User.class.php
@@ -8,6 +8,7 @@ class User
     private $Lastname;
     private $Email;
     private $Password;
+    private $Admin;
 
     // FIRSTNAME
     public function getFirstname()
@@ -65,19 +66,47 @@ class User
         $this->Password = $Password;
     }
 
+    // ADMIN
+    public function getAdmin()
+    {
+        return $this->Admin;
+    }
+
+    public function setAdmin($Admin)
+    {
+        if ($Admin=="") {
+            throw new Exception('Admin can not be empty');
+        }
+        $this->Admin = $Admin;
+    }
+
     // REGISTER
     public function register()
     {
         // connection
         global $conn;
 
+        if (!filter_var($this->Email, FILTER_VALIDATE_EMAIL)) {
+            throw new Exception($this->Email . " is not a valid email address");
+
+          }
+
+          $query = $conn->prepare( "SELECT email FROM user WHERE email = :email");
+          $query->bindValue(":email", $this->Email);
+          $query->execute();
+          $query->fetch(PDO::FETCH_ASSOC);
+  
+          if($query->rowCount() !== 0) { # If rows are found for query
+              throw new Exception("Email already exists"); 
+          }
         // Gegevens in de databank plaatsen
-        $statement = $conn->prepare("insert into user (firstname, lastname, email, password) values (:firstname, :lastname, :email, :password)");
+        $statement = $conn->prepare("insert into user (firstname, lastname, email, password, isAdmin) values (:firstname, :lastname, :email, :password, :isAdmin)");
         // Uitvoeren van een prepared statement (bindparam)
         $statement->bindparam(":firstname", $this->Firstname); 
         $statement->bindparam(":lastname", $this->Lastname);
         $statement->bindparam(":email", $this->Email);
         $statement->bindparam(':password', $this->Password);
+        $statement->bindparam(':isAdmin', $this->Admin);
 
        /* 
        if( strlen($password) > 8){
@@ -85,18 +114,42 @@ class User
         }
         */
 
+
+    // WACHTWOORD BEVEILIGEN MET BCRYPT
         $options = [
-            'cost'=> 12
+            'cost'=> 12 //salt van 12
         ];
         $password = password_hash($this->getPassword(), PASSWORD_DEFAULT, $options);
         $statement->bindValue(":password", $password);
-        $statement->execute();
 
         // execute (uitvoeren)
         $result = $statement->execute();
 
         // return true/false
         return $result;
+    }
+
+
+    // EMAIL NAKIJKEN OF ER AL 1 BESTAAT 
+    public function checkIfEmailExists($Email){
+
+        // connection
+         global $conn;
+
+       
+
+
+
+    }
+
+    public function checkIsAdmin()
+    {
+        global $conn;
+
+        $statement = $conn->prepare("SELECT isAdmin FROM user where email = :email");
+        $statement->bindValue(":email", $_SESSION['email']);
+        $statement->execute();
+        return $statement;
     }
 
     // LOGIN

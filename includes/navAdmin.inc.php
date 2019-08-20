3006ecf286db83706625d9b2e4cdb08b150fb8ed
CedricRaddoux <r0669133@student.thomasmore.be>
2019-08-20 21:44:41 +0200

    Create navAdmin.inc.php

---
+++ includes/navAdmin.inc.php
@@ -0,0 +1,8 @@
+<nav class=header_nav>
+    <a class="nav_logo" href="index.php"><img src="images/logo.png" alt="Todoloe logo" style="height:50px;"></a>
+    <a class="nav_link" href="list.php">Add list</a>
+    <a class="nav_link" href="task.php">Add task</a>
+    <a class="nav_link" href="addAdmin.php">Add admin</a>
+    <a class="nav_link" href="deleteAdmin.php">Delete admin</a>
+    <a class="nav_link" href="includes/logout.inc.php">Logout</a>
+</nav>

caf0f5c6b3122c6ad06ba369cf1160d62085b4af
CedricRaddoux <r0669133@student.thomasmore.be>
2019-08-20 21:44:44 +0200

    Update indexAdmin.php

---
+++ indexAdmin.php
@@ -0,0 +1,28 @@
+<?php
+    session_start();
+        if( !isset($_SESSION['email'])){ //als het niet ge set is
+           header('location: login.php'); 
+        }
+
+?><!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Todoloe</title>
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Karla|Lexend+Deca&display=swap" rel="stylesheet">
+    
+</head>
+<body>
+<?php include_once("includes/navAdmin.inc.php"); ?>
+
+<h1>Welcome back <?php echo $_SESSION['email']; ?></h1>
+
+
+
+</body>
+</html>

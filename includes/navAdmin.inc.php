aee034c9ced9993ae7541f377ad661e3d365808f
CedricRaddoux <r0669133@student.thomasmore.be>
2019-08-21 16:06:33 +0200

    Update navAdmin.inc.php

--- includes/navAdmin.inc.php
+++ includes/navAdmin.inc.php
@@ -1,8 +1,7 @@
 <nav class=header_nav>
     <a class="nav_logo" href="index.php"><img src="images/logo.png" alt="Todoloe logo" style="height:50px;"></a>
-    <a class="nav_link" href="list.php">Add list</a>
-    <a class="nav_link" href="task.php">Add task</a>
-    <a class="nav_link" href="addAdmin.php">Add admin</a>
-    <a class="nav_link" href="deleteAdmin.php">Delete admin</a>
+    <a class="nav_link" href="list.php">Lists &#38; tasks</a>
+    <a class="nav_link" href="seeAdmin.php">Admins</a>
+    <a class="nav_link" href="seeUser.php">Users</a>
     <a class="nav_link" href="includes/logout.inc.php">Logout</a>
 </nav>

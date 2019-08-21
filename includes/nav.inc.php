0dcb2b762f00ccdf9c3842599a0791160c5f22f9
CedricRaddoux <r0669133@student.thomasmore.be>
2019-08-21 16:06:29 +0200

    Update nav.inc.php

--- includes/nav.inc.php
+++ includes/nav.inc.php
@@ -1,6 +1,5 @@
 <nav class=header_nav>
     <a class="nav_logo" href="index.php"><img src="images/logo.png" alt="Todoloe logo" style="height:50px;"></a>
-    <a class="nav_link" href="list.php">Add list</a>
-    <a class="nav_link" href="task.php">Add task</a>
+    <a class="nav_link" href="list.php">Lists &#38; tasks</a>
     <a class="nav_link" href="includes/logout.inc.php">Logout</a>
 </nav>
